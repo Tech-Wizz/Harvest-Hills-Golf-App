69b75d9870daabab553c67a6dd0704b79ad3fdc1
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-03-29 14:02:12 -0600

    Added Base Project

---
+++ Harvest Hills Golf/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Harvest Hills Golf
+//
+//  Created by Kruize Christensen on 3/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
