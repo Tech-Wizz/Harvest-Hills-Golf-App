676239845e7cfee772384fdccceb836684cfcfb5
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-03-29 21:06:53 -0600

    Update with TabView

---
+++ Harvest Hills Golf/Controller/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  Harvest Hills Golf
+//
+//  Created by Kruize Christensen on 3/29/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+    }
+
+
+}
