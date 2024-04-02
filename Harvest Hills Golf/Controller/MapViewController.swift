e302825daa050f74ccd330253f3dca1d73b93b3c
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-04-02 10:05:48 -0600

    Add Data Manipulation
    Mimicking MyTrips from StewartLynch

--- Harvest Hills Golf/Controller/MapViewController.swift
+++ Harvest Hills Golf/Controller/MapViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    let minLatitude = 47.60075
+    let maxLatitude = 47.60615
+    let minLongitude = -111.98539
+    let maxLongitude = -111.97588
+    
+    let minZoomLevel: CLLocationDistance = 5
+    let maxZoomLevel: CLLocationDistance = 30
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        
+        let mapView = MKMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.delegate = self
+        view.addSubview(mapView)
+        
+        // Initial region setup
+        let center = CLLocationCoordinate2D(latitude: (minLatitude + maxLatitude) / 2, longitude: (minLongitude + maxLongitude) / 2)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: maxZoomLevel, longitudinalMeters: maxZoomLevel)
+        mapView.setRegion(mapView.regionThatFits(region), animated: true)
     }
-
-
+    
 }
---
+++ Harvest Hills Golf/DestinationLocationsMapView.swift
@@ -0,0 +1,45 @@
+//
+//  DestinationLocationsMapView.swift
+//  Harvest Hills Golf
+//
+//  Created by Kruize Christensen on 4/2/24.
+//
+
+
+
+import SwiftUI
+import MapKit
+import SwiftData
+
+struct DestinationLocationsMapView: View {
+    @State private var cameraPosition: MapCameraPosition = .automatic
+    @State private var visibleRegion: MKCoordinateRegion?
+    @Query private var destinations: [Destination]
+    @State private var destination: Destination?
+    var body: some View {
+        Map(position: $cameraPosition) {
+            if let destination {
+                ForEach(destination.placemarks) { placemark in
+                    Marker(coordinate: placemark.coordinate) {
+                        Label(placemark.number, systemImage: "star")
+                    }
+                    .tint(.yellow)
+                }
+            }
+        }
+        .onMapCameraChange(frequency: .onEnd){ context in
+            visibleRegion = context.region
+        }
+        .onAppear {
+            destination = destinations.first
+            if let region = destination?.region {
+                cameraPosition = .region(region)
+            }
+        }
+    }
+}
+
+#Preview {
+    DestinationLocationsMapView()
+        .modelContainer(Destination.preview)
+}
